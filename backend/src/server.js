62535256b9e80f64edbbc050ef86d15db1320514
Komal4567 <komalsinghsep04@gmail.com>
2026-04-19 00:57:05 +0530

    Fixed server.js for production

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,6 @@ import express from "express";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
@@ -11,16 +10,15 @@ import chatRoutes from "./routes/chat.route.js";
 import { connectDB } from "./lib/db.js";
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5001;
 
-const __dirname = path.resolve();
-
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true, // allow frontend to send cookies
-  })
-);
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://streamify-psi-eight.vercel.app"
+  ],
+  credentials: true,
+}));
 
 app.use(express.json());
 app.use(cookieParser());
@@ -28,14 +26,6 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
