6e44e8568dfa7058dc757c41df851ffa27a2cb8d
Komal4567 <komalsinghsep04@gmail.com>
2026-04-23 15:37:57 +0530

    Fixed server.js AI route

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,12 +2,11 @@ import express from "express";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import aiRoutes from "./routes/ai.route.js";
-app.use("/api/ai", aiRoutes);
 
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import chatRoutes from "./routes/chat.route.js";
+import aiRoutes from "./routes/ai.route.js";
 
 import { connectDB } from "./lib/db.js";
 
@@ -28,6 +27,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
+app.use("/api/ai", aiRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
